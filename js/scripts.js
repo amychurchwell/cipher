94847c6397b96935e95d93c45f8329736fb50d40
Amy Churchwell and Cat Janowitz <amy@>
2017-10-16 15:47:43 -0700

    add initial index and scripts and jquery files

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+var input = prompt('Type in a sentence:');
+console.log(input);
+
+var firstLast = function(input) {
+  return input.charAt(3);
+};
+
+alert(firstLast(input));
+
+var capitalize = function(firstLast) {
+  return firstLast.toUpperCase();
+}
+
+alert(capitalize + something);
